69ebf963a663e2c8af5f6b85586367a4e6c98723
Raushan Kumar <113075873+raushanraie01@users.noreply.github.com>
2025-10-01 18:26:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let data=document.querySelectorAll(".price");
+	let table=document.querySelector("table")
+	let row=document.createElement("tr");
+let totalPrice=	data.reduce((acc,curr)=>acc+curr,0)
+	row.innerText(totalPrice);
+	table.append(row)
   
 };
 
